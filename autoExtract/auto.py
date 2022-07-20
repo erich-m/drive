486cd95d0c55c86f02673e71773f008ae2cc5dc5
Erich MacLean <erich.m.maclean@gmail.com>
2022-07-20 10:16:33 -0400

    added command to open analysis tool

--- autoExtract/auto.py
+++ autoExtract/auto.py
@@ -2,7 +2,11 @@ import wmi
 
 ANALYSING_TOOL = "AnalysingTool"
 
-w = wmi.WMI()
+# w = wmi.WMI()
+# processes = ANALYSING_TOOL in [f"{p.Name}".replace(".exe","") for p in w.Win32_Process()]#check if the Analysis tool is running or not
+# print(processes)
 
-processes = ANALYSING_TOOL in [f"{p.Name}".replace(".exe","") for p in w.Win32_Process()]
-print(processes)
+
+# C:\OKTAL\SCANeRstudio_1.6\bin\x64\AnalysingTool.exe -c DRIVER_DS_1.6 C:/OKTAL/SCANeRstudio_1.6/data/GUELPH_DATA_1.6/record/MTO_MWPractice - Copy-31_01_2018-17h54m37s/MTO_MWPractice - Copy-31_01_2018-17h54m37s.recprj
+
+#AnalysingTool.exe -c DRIVER_DS_1.6 "C:/OKTAL/SCANeRstudio_1.6/data/GUELPH_DATA_1.6/record/MTO_MWPractice - Copy-31_01_2018-17h54m37s/MTO_MWPractice - Copy-31_01_2018-17h54m37s.recprj"
