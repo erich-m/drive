067acf246a8cadb9fe2c77a356129650b351c6b8
Erich MacLean <erich.m.maclean@gmail.com>
2022-07-20 15:27:57 -0400

    modded

--- autoExtract/auto.py
+++ autoExtract/auto.py
@@ -1,11 +1,19 @@
+import os
+import signal
+
 import wmi
 
 ANALYSING_TOOL = "AnalysingTool"
 
-# w = wmi.WMI()
-# processes = ANALYSING_TOOL in [f"{p.Name}".replace(".exe","") for p in w.Win32_Process()]#check if the Analysis tool is running or not
-# print(processes)
+w = wmi.WMI()
+appNames = [f"{p.Name}".replace(".exe","") for p in w.Win32_Process()]#check if the Analysis tool is running or not
+appPids = [f"{p.ProcessId}" for p in w.Win32_Process()]#check if the Analysis tool is running or not
 
+apps = dict(zip(appNames,appPids))
+
+
+
+print(apps)
 
 # C:\OKTAL\SCANeRstudio_1.6\bin\x64\AnalysingTool.exe -c DRIVER_DS_1.6 C:/OKTAL/SCANeRstudio_1.6/data/GUELPH_DATA_1.6/record/MTO_MWPractice - Copy-31_01_2018-17h54m37s/MTO_MWPractice - Copy-31_01_2018-17h54m37s.recprj
 
