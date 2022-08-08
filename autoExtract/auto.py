c030e1c6cdfb547d671800eff01bcd156e172889
Erich MacLean <erich.m.maclean@gmail.com>
2022-08-07 22:03:18 -0400

    added home computer exe command

--- autoExtract/auto.py
+++ autoExtract/auto.py
@@ -18,3 +18,5 @@ print(apps)
 # C:\OKTAL\SCANeRstudio_1.6\bin\x64\AnalysingTool.exe -c DRIVER_DS_1.6 C:/OKTAL/SCANeRstudio_1.6/data/GUELPH_DATA_1.6/record/MTO_MWPractice - Copy-31_01_2018-17h54m37s/MTO_MWPractice - Copy-31_01_2018-17h54m37s.recprj
 
 #AnalysingTool.exe -c DRIVER_DS_1.6 "C:/OKTAL/SCANeRstudio_1.6/data/GUELPH_DATA_1.6/record/MTO_MWPractice - Copy-31_01_2018-17h54m37s/MTO_MWPractice - Copy-31_01_2018-17h54m37s.recprj"
+
+# C:\OKTAL\SCANeRstudio_1.6\bin\x64\AnalysingTool.exe -c GUELPH_DATA_1.6 C:/OKTAL/SCANeRstudio_1.6/data/DEFAULT/record/test-07_08_2022-16h33m55s/test-07_08_2022-16h33m55s.recprj
