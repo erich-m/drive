f3fb9d66dcf890dfce31a73bb8973215e5e96b44
Erich MacLean <erich.m.maclean@gmail.com>
2022-10-21 23:07:05 -0400

    made slight fix to export.py

--- export.py
+++ export.py
@@ -404,6 +404,7 @@ class Window:
             messagebox.showinfo('State Saved Successfully','Current configuration saved to file')
         elif stateready == (False,False):
             messagebox.showerror('State Save Failed','State save failed due to unknown errors')
+
 auth = -1
 def initpath():#initialize the path to the directory that SCANeR Studio uses
     if str(os.environ['COMPUTERNAME']) == 'SUPERVISION':
@@ -439,4 +440,5 @@ root.resizable(False,False)
 root.title(config.get('guidata','main'))
 root.iconbitmap("export.ico")
 root.minsize(config.get('guidata','width'),config.get('guidata','height'))
+root.protocol("WM_DELETE_WINDOW", oncloseevent)
 root.mainloop()
